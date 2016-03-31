654cedc8bd283b1477571711d91090f12c27c46b
Rui Tang <tangrui.cn@gmail.com>
2016-04-01 07:04:34 +0800

    Add config feature and related resource files and test cases.

---
+++ src/config/__mocks__/index.js
@@ -0,0 +1,9 @@
+const config = {
+  sysPath: '/path/to/sys',
+  appPath: '/path/to/app'
+}
+
+export default {
+  set: jest.genMockFunction().mockImplementation((key, value) => (config[key] = value)),
+  get: jest.genMockFunction().mockImplementation(key => config[key])
+}
---
+++ src/config/index.js
@@ -0,0 +1,22 @@
+import path from 'path'
+
+import {Provider} from 'nconf/lib/nconf/provider'
+
+const environment = process.env.NODE_ENV || 'development'
+const appPath = process.cwd()
+
+const nconf = new Provider()
+
+nconf
+  .argv()
+  .env()
+  .file('app_env', {file: path.join(appPath, 'config', `${environment}.json`)})
+  .file('sys_env', {file: path.join(__dirname, `${environment}.json`)})
+  .file('app_default', {file: path.join(appPath, 'config', 'default.json')})
+  .file('sys_default', {file: path.join(__dirname, 'default.json')})
+
+nconf.set('environment', environment)
+nconf.set('sysPath', path.join(__dirname, '..', '..'))
+nconf.set('appPath', appPath)
+
+export default nconf
