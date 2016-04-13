9f29fd2e8e89ea694821648a0797b19ba6703826
Rui Tang <tangrui.cn@gmail.com>
2016-04-13 22:44:58 +0800

    Sketch structure now cannot be customized.

--- src/server/__tests__/sketch-test.js
+++ src/server/__tests__/sketch-test.js
@@ -4,34 +4,10 @@ describe('sketch', () => {
   const Sketch = require('../sketch').default
 
   it('should take default option values', () => {
-    const s = new Sketch({
-      name: 'test-sketch'
-    })
-    s.isNpmSkech = false
+    const s = new Sketch('test-sketch')
+    s.sketchType = Sketch.TYPE_LOCAL
 
     expect(s.name).toBe('test-sketch')
-    expect(s.modelDirectory).toBe('lib/models')
-    expect(s.typeDirectory).toBe('lib/graphql/types')
-    expect(s.queryDirectory).toBe('lib/graphql/queries')
-    expect(s.mutationDirectory).toBe('lib/graphql/mutations')
-    expect(s.isNpmSkech).toBe(false)
-  })
-
-  it('should take input option values', () => {
-    const s = new Sketch({
-      name: 'test-sketch',
-      modelDirectory: 'path/to/models',
-      typeDirectory: 'path/to/types',
-      queryDirectory: 'path/to/queries',
-      mutationDirectory: 'path/to/mutations'
-    })
-    s.isNpmSkech = true
-
-    expect(s.name).toBe('test-sketch')
-    expect(s.modelDirectory).toBe('path/to/models')
-    expect(s.typeDirectory).toBe('path/to/types')
-    expect(s.queryDirectory).toBe('path/to/queries')
-    expect(s.mutationDirectory).toBe('path/to/mutations')
-    expect(s.isNpmSkech).toBe(true)
+    expect(s.sketchType).toBe(Sketch.TYPE_LOCAL)
   })
 })
--- src/server/sketch.js
+++ src/server/sketch.js
@@ -1,43 +1,29 @@
+// import ModelRepository from './model-repository'
+
 export default class Sketch {
-  constructor({
-    name,
-    modelDirectory = 'lib/models',
-    typeDirectory = 'lib/graphql/types',
-    queryDirectory = 'lib/graphql/queries',
-    mutationDirectory = 'lib/graphql/mutations'
-  }) {
+  static TYPE_NPM = Symbol('npm')
+  static TYPE_LOCAL = Symbol('local')
+
+  static directories = {
+    models: 'lib/models',
+    types: 'lib/graphql/types',
+    queries: 'lib/graphql/queries',
+    mutations: 'lib/graphql/mutations'
+  }
+
+  constructor(name) {
     this._name = name
-    this._modelDirectory = modelDirectory
-    this._typeDirectory = typeDirectory
-    this._queryDirectory = queryDirectory
-    this._mutationDirectory = mutationDirectory
   }
 
   get name() {
     return this._name
   }
 
-  get modelDirectory() {
-    return this._modelDirectory
+  set sketchType(sketchType) {
+    this._sketchType = sketchType
   }
 
-  get typeDirectory() {
-    return this._typeDirectory
-  }
-
-  get queryDirectory() {
-    return this._queryDirectory
-  }
-
-  get mutationDirectory() {
-    return this._mutationDirectory
-  }
-
-  set isNpmSkech(_isNpmSketch) {
-    this._isNpmSketch = _isNpmSketch
-  }
-
-  get isNpmSkech() {
-    return this._isNpmSketch
+  get sketchType() {
+    return this._sketchType
   }
 }
